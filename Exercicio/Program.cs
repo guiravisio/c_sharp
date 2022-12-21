0ed19e7a99b436978bf781e8bc4e22c0df037df6
guiravisio <guilherme.ravisio@gmail.com>
2022-12-21 10:15:33 -0300

    Implementação do cáculo da média e do conceito

--- Exercicio/Program.cs
+++ Exercicio/Program.cs
@@ -8,7 +8,7 @@
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1 - Inserir novo aluno");
             Console.WriteLine("2 - Listar alunos");
-            Console.WriteLine("3 - Calcular média");
+            Console.WriteLine("3 - Calcular média e conceito geral da turma");
             Console.WriteLine("X - Sair");
             Console.WriteLine();
 
@@ -51,6 +51,36 @@
                         break;
 
                     case "3": //calcular média
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+                        for(int i = 0; i < alunos.Length; i++){
+                             if (!string.IsNullOrEmpty(alunos[i].Nome)){
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+
+                            
+                        }
+
+                        decimal mediaGeral = notaTotal/nrAlunos;
+                        Conceito conceitoGeral;
+                        if(mediaGeral < 2){
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if(mediaGeral < 4){
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if(mediaGeral < 6){
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if(mediaGeral < 8){
+                            conceitoGeral = Conceito.B;
+                        }
+                        else{
+                            conceitoGeral = Conceito.A;
+                        }
+
+                        Console.WriteLine($"Média geral: {mediaGeral} - Conceito: {conceitoGeral}");
 
                         break;
 
